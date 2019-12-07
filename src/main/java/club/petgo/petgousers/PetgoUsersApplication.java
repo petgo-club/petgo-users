befe7342e8148b1c806361112e82aed0ffe6ecd8
Urte <urte.rimsaite@gmail.com>
2019-12-07 17:11:31 +0200

    Initial commit

---
+++ src/main/java/club/petgo/petgousers/PetgoUsersApplication.java
@@ -0,0 +1,13 @@
+package club.petgo.petgousers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetgoUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetgoUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/petgo/petgousers/PetgoUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package club.petgo.petgousers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetgoUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
